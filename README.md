a5578ba3fcf0dbb83ed9e048d398e547269fe6e7
Scott Johnstone <scottajohnstone@gmail.com>
2024-06-28 14:36:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Combat Tracker
+
+A simple RPG combat and initiative tracker that stores some starter Heroes and Villains in a table and allows the user to edit/delete each character (row) or add new ones.
+The characters should all be sorted by initiative value in descending order.
+
+<!-- TODO list and describe each feature of the app -->
---
+++ docs/data-mockup.js
@@ -0,0 +1,9 @@
+[
+    { id: 0, name: 'Drax', initiative: 0, armor: 18, minHP: 50, maxHP: 50, hero: true },
+    { id: 1, name: 'Gamora', initiative: 0, armor: 15, minHP: 40, maxHP: 40, hero: true },
+    { id: 2, name: 'Rocket', initiative: 0, armor: 13, minHP: 30, maxHP: 30,  hero: true },
+    { id: 3, name: 'Star Lord', initiative: 0, armor: 16, minHP: 35, maxHP: 35, hero: true },
+    { id: 4, name: 'Thanos', initiative: 0, armor: 20, minHP: 150,  maxHP: 150, hero: false },
+    { id: 5, name: 'Ronan', initiative: 0, armor: 17, minHP: 75, maxHP: 75, hero: false },
+]
+    
